311e770b9ce181819ad6e460400064e4b40cbb91
Ancelbot <135066230+Ancelbot@users.noreply.github.com>
2023-11-13 21:50:57 -0500

    s

--- plugins/info-estado.js
+++ plugins/info-estado.js
@@ -5,16 +5,16 @@ const handler = async (m, {conn}) => {
     const _uptime = process.uptime() * 1000;
     const uptime = clockString(_uptime);
     const taguser = '@' + m.sender.split('@s.whatsapp.net')[0];
-    const str = `╭────[ *𝕵𝖔𝖘𝖘 𝕭𝖔𝖙 🏴‍☠️* ]
-│
-│ *➤ ʜᴏʟᴀ ${taguser}*
-│
-│ *=> 🤖 ᴛɪᴇᴍᴘᴏ ᴀᴄᴛɪᴠᴏ:* ${uptime}
-│ *=> ✅ ʙᴏᴛ ᴅᴇ ᴜsᴏ ᴘᴜʙʟɪᴄᴏ*
-│ *=> 👑 ᴄʀᴇᴀᴅᴏʀ: 𝕵𝖔𝖘𝖘 𝕭𝖔𝖙 🏴‍☠️*
-│ *=> 🔗 ᴄᴜᴇɴᴛᴀs ᴏғᴄ:* wa.me/56986469687
-│ *=> 🔗 ᴄᴜᴇɴᴛᴀs ᴏғᴄ:* https://www.instagram.com/ancelzzz/
-╰────────────────`.trim();
+    const str = `╭────[ 𝕵𝖔𝖘𝖘 𝕭𝖔𝖙 🏴‍☠️ ]
+    │
+    │ ➤ ʜᴏʟᴀ ${taguser}
+    │
+    │ => 🤖 ᴛɪᴇᴍᴘᴏ ᴀᴄᴛɪᴠᴏ: ${uptime}
+    │ => ✅ ʙᴏᴛ ᴅᴇ ᴜsᴏ ᴘᴜʙʟɪᴄᴏ
+    │ => 👑 ᴄʀᴇᴀᴅᴏʀ: 𝕵𝖔𝖘𝖘 𝕭𝖔𝖙 🏴‍☠️
+    │ => 🔗 ᴄᴜᴇɴᴛᴀs ᴏғᴄ: wa.me/56986469687
+    │ => 🔗 ᴄᴜᴇɴᴛᴀs ᴏғᴄ: https://www.instagram.com/ancelzzz/
+    ╰────────────────`.trim();
     if (m.isGroup) {
       conn.sendMessage(m.chat, {text: str.trim(), mentions: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net'), contextInfo: {forwardingScore: 9999999, isForwarded: true, mentionedJid: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net'), "externalAdReply": {"showAdAttribution": true, "containsAutoReply": true, "renderLargerThumbnail": true, "title": global.titulowm, "containsAutoReply": true, "mediaType": 1, "thumbnail": pp, "mediaUrl": `https://www.atom.bio/theshadowbrokers-team`, "sourceUrl": `https://www.atom.bio/theshadowbrokers-team`}}}, {quoted: m});
     } else {
@@ -29,9 +29,3 @@ handler.tags = ['main'];
 handler.command = /^(estado|status|estate|state|stado|stats|runtime|uptime)$/i;
 export default handler;
 function clockString(ms) {
-  const d = isNaN(ms) ? '--' : Math.floor(ms / 86400000);
-  const h = isNaN(ms) ? '--' : Math.floor(ms / 3600000) % 24;
-  const m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60;
-  const s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60;
-  return [`\n│ *=> 💥 ` + d, ' Día(s)* ', `\n│ *=> 💫 ` + h, ' Hora(s)* ', `\n│ *=> 💠 ` + m, ' Minuto(s)* ', `\n│ *=> ♦ ` + s, ' Segundo(s)* '].map((v) => v.toString().padStart(2, 0)).join('');
-}

393468470b911dedc834b4e39b8912f74d615597
Ancelbot <135066230+Ancelbot@users.noreply.github.com>
2023-11-13 21:53:06 -0500

    Update info-estado.js

--- plugins/info-estado.js
+++ plugins/info-estado.js
@@ -13,7 +13,6 @@ const handler = async (m, {conn}) => {
     │ => ✅ ʙᴏᴛ ᴅᴇ ᴜsᴏ ᴘᴜʙʟɪᴄᴏ
     │ => 👑 ᴄʀᴇᴀᴅᴏʀ: 𝕵𝖔𝖘𝖘 𝕭𝖔𝖙 🏴‍☠️
     │ => 🔗 ᴄᴜᴇɴᴛᴀs ᴏғᴄ: wa.me/56986469687
-    │ => 🔗 ᴄᴜᴇɴᴛᴀs ᴏғᴄ: https://www.instagram.com/ancelzzz/
     ╰────────────────`.trim();
     if (m.isGroup) {
       conn.sendMessage(m.chat, {text: str.trim(), mentions: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net'), contextInfo: {forwardingScore: 9999999, isForwarded: true, mentionedJid: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net'), "externalAdReply": {"showAdAttribution": true, "containsAutoReply": true, "renderLargerThumbnail": true, "title": global.titulowm, "containsAutoReply": true, "mediaType": 1, "thumbnail": pp, "mediaUrl": `https://www.atom.bio/theshadowbrokers-team`, "sourceUrl": `https://www.atom.bio/theshadowbrokers-team`}}}, {quoted: m});

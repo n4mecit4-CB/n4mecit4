a8b13166650d7cbbb69c170c6b0fc02c0bcebdc2
n4mecit4-CB <151418600+n4mecit4-CB@users.noreply.github.com>
2023-11-19 05:45:36 -0300

    Ɲ4MЄƇƖƬ4_ƁƇ

--- plugins/info-gruposofc.js
+++ plugins/info-gruposofc.js
@@ -1,31 +1,31 @@
 const handler = async (m, {conn, usedPrefix}) => {
   const doc = ['pdf', 'zip', 'vnd.openxmlformats-officedocument.presentationml.presentation', 'vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'vnd.openxmlformats-officedocument.wordprocessingml.document'];
   const document = doc[Math.floor(Math.random() * doc.length)];
-  const text = `*𝙷𝙾𝙻𝙰 𝚄𝚂𝚄𝙰𝚁𝙸𝙾 👋🏻, 𝚃𝙴 𝙸𝙽𝚅𝙸𝚃𝙾 𝙰 𝚄𝙽𝙸𝚁𝚃𝙴 𝙰 𝙻𝙾𝚂 𝙶𝚁𝚄𝙿𝙾𝚂 𝙾𝙵𝙸𝙲𝙸𝙰𝙻𝙴𝚂 𝙳𝙴 𝕵𝖔𝖘𝖘 𝕭𝖔𝖙 🏴‍☠️  -  𝙿𝙰𝚁𝙰 𝙲𝙾𝙽𝚅𝙸𝚅𝙸𝚁 𝙲𝙾𝙽 𝙻𝙰 𝙲𝙾𝙼𝚄𝙽𝙸𝙳𝙰𝙳 :D*
+  const text = `*𝙷𝙾𝙻𝙰 𝚄𝚂𝚄𝙰𝚁𝙸𝙾 👋🏻, 𝚃𝙴 𝙸𝙽𝚅𝙸𝚃𝙾 𝙰 𝚄𝙽𝙸𝚁𝚃𝙴 𝙰 𝙻𝙾𝚂 𝙶𝚁𝚄𝙿𝙾𝚂 𝙾𝙵𝙸𝙲𝙸𝙰𝙻𝙴𝚂 𝙳𝙴 Ɲ4MЄƇƖƬ4_ƁƇ  -  𝙿𝙰𝚁𝙰 𝙲𝙾𝙽𝚅𝙸𝚅𝙸𝚁 𝙲𝙾𝙽 𝙻𝙰 𝙲𝙾𝙼𝚄𝙽𝙸𝙳𝙰𝙳 :D*
 
-*➤ Info grupos 𝕵𝖔𝖘𝖘 𝕭𝖔𝖙 🏴‍☠️ :*
-*1.-* Hablar: wa.me/56986469687
+*➤ Info grupos Ɲ4MЄƇƖƬ4_ƁƇ :*
+*1.-* Hablar: wa.me/56922396080
 
-*2.-* https://chat.whatsapp.com/KGjoATft9FU00wueIKLjtN
+*2.-* https://chat.whatsapp.com/LSmz0Q0eCit0SzyCQn4dMd
 
-*3.-* https://www.instagram.com/ancelzzz/`.trim();
+*3.-* https://www.instagram.com/n4mecit4_bc/`.trim();
   const buttonMessage= {
-    'document': {url: `https://www.instagram.com/ancelzzz/`},
+    'document': {url: `https://www.instagram.com/n4mecit4_bc/`},
     'mimetype': `application/${document}`,
-    'fileName': `「  𝕵𝖔𝖘𝖘 𝕭𝖔𝖙 」`,
+    'fileName': `「  Ɲ4MЄƇƖƬ4_ƁƇ 」`,
     'fileLength': 99999999999999,
     'pageCount': 200,
     'contextInfo': {
       'forwardingScore': 200,
       'isForwarded': true,
       'externalAdReply': {
-        'mediaUrl': 'https://www.instagram.com/ancelzzz/',
+        'mediaUrl': 'https://www.instagram.com/n4mecit4_bc/',
         'mediaType': 2,
         'previewType': 'pdf',
         'title': 'ᴇʟ ᴍᴇᴊᴏʀ ʙᴏᴛ ᴅᴇ ᴡʜᴀᴛsᴀᴘᴘ⁩',
         'body': wm,
         'thumbnail': imagen1,
-        'sourceUrl': 'https://www.instagram.com/ancelzzz/'}},
+        'sourceUrl': 'https://www.instagram.com/n4mecit4_bc/'}},
     'caption': text,
     'footer': wm,
     'headerType': 6};

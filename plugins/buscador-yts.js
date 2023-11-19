10f09712d12f1eb96925429be037573a9b391f0d
n4mecit4-CB <151418600+n4mecit4-CB@users.noreply.github.com>
2023-11-19 04:57:21 -0300

    Ɲ4MЄƇƖƬ4_ƁƇ

--- plugins/buscador-yts.js
+++ plugins/buscador-yts.js
@@ -2,7 +2,7 @@ import yts from 'yt-search';
 import fs from 'fs';
 
 const handler = async (m, {conn, text}) => {
-  if (!text) throw '⚠️ *_Que quieres que busque en YouTube?_*';
+  if (!text) throw '⚠️ *_𝘘𝘶𝘦 𝘲𝘶𝘪𝘦𝘳𝘦𝘴 𝘲𝘶𝘦 𝘣𝘶𝘴𝘲𝘶𝘦 𝘦𝘯 𝘠𝘰𝘶𝘛𝘶𝘣𝘦?_*';
   const results = await yts(text);
   const tes = results.all;
   const teks = results.all.map((v) => {

cb8e82ed9c732efe726d2789193c425a38f11434
n4mecit4-CB <151418600+n4mecit4-CB@users.noreply.github.com>
2023-11-19 05:39:00 -0300

    Ɲ4MЄƇƖƬ4_ƁƇ

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -1,30 +1,30 @@
 const handler = async (m, {conn, usedPrefix}) => {
   const doc = ['pdf', 'zip', 'vnd.openxmlformats-officedocument.presentationml.presentation', 'vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'vnd.openxmlformats-officedocument.wordprocessingml.document'];
   const document = doc[Math.floor(Math.random() * doc.length)];
-  const text = `*—◉ 𝙴𝙻 𝙽𝚄𝙼𝙴𝚁𝙾 𝙳𝙴 𝙼𝙸 𝙲𝚁𝙴𝙰𝙳𝙾𝚁 𝙴𝚂 wa.me/56986469687*
+  const text = `*—◉ 𝙴𝙻 𝙽𝚄𝙼𝙴𝚁𝙾 𝙳𝙴 𝙼𝙸 𝙲𝚁𝙴𝙰𝙳𝙾𝚁 𝙴𝚂 wa.me/56936399966*
 
-*—◉ 𝙴𝙻 𝙽𝚄𝙼𝙴𝚁𝙾 𝙳𝙴𝙻 𝙲𝙾𝙻𝙰𝙱𝙾𝚁𝙰𝙳𝙾𝚁  𝙴𝚂 wa.me/56986469687*
+*—◉ 𝙴𝙻 𝙽𝚄𝙼𝙴𝚁𝙾 𝙳𝙴𝙻 𝙲𝙾𝙻𝙰𝙱𝙾𝚁𝙰𝙳𝙾𝚁  𝙴𝚂 wa.me/56922396080*
 
-*—◉ 𝙴𝙻 𝙲𝙾𝙽𝚃𝙰𝙲𝚃𝙾 𝙳𝙴 IG 𝙴𝚂 https://instagram.com/ancelzzz*
+*—◉ 𝙴𝙻 𝙲𝙾𝙽𝚃𝙰𝙲𝚃𝙾 𝙳𝙴 IG 𝙴𝚂 https://instagram.com/n4mecit4_bc*
 
-*—◉ 𝙴𝙻 𝙽𝚄𝙼𝙴𝚁𝙾 𝙳𝙴 MI CREADOR POR SI GUSTA 𝙴𝚂 wa.me/56986469687*`.trim();
+*—◉ 𝙴𝙻 𝙽𝚄𝙼𝙴𝚁𝙾 𝙳𝙴 MI CREADOR POR SI GUSTA 𝙴𝚂 wa.me/56922396080*`.trim();
   const buttonMessage= {
-    'document': {url: `https://instagram.com/ancelzzz`},
+    'document': {url: `https://instagram.com/n4mecit4_bc`},
     'mimetype': `application/${document}`,
-    'fileName': `「 𝕵𝖔𝖘𝖘 𝕭𝖔𝖙 🏴‍☠️」`,
+    'fileName': `「 Ɲ4MЄƇƖƬ4_ƁƇ」`,
     'fileLength': 99999999999999,
     'pageCount': 200,
     'contextInfo': {
       'forwardingScore': 200,
       'isForwarded': true,
       'externalAdReply': {
-        'mediaUrl': 'https://instagram.com/ancelzzz',
+        'mediaUrl': 'https://instagram.com/n4mecit4_bc',
         'mediaType': 2,
         'previewType': 'pdf',
         'title': 'ᴇʟ ᴍᴇᴊᴏʀ ʙᴏᴛ ᴅᴇ ᴡʜᴀᴛsᴀᴘᴘ⁩',
         'body': wm,
         'thumbnail': imagen1,
-        'sourceUrl': 'https://instagram.com/ancelzzz'}},
+        'sourceUrl': 'https://instagram.com/n4mecit4_bc'}},
     'caption': text,
     'footer': wm,
     // 'buttons':[

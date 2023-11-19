694bdf1d86eef9ad0e2c562730e1bff2ea153370
n4mecit4-CB <151418600+n4mecit4-CB@users.noreply.github.com>
2023-11-19 03:51:33 -0300

    Ɲ4MЄƇƖƬ4_ƁƇ

--- plugins/_antiprivado.js
+++ plugins/_antiprivado.js
@@ -6,7 +6,7 @@ export async function before(m, {conn, isAdmin, isBotAdmin, isOwner, isROwner}) 
   const chat = global.db.data.chats[m.chat];
   const bot = global.db.data.settings[this.user.jid] || {};
   if (bot.antiPrivate && !isOwner && !isROwner) {
-    await m.reply(`*[❗] ʜᴏʟᴀ @${m.sender.split`@`[0]}, ᴇsᴛᴀ ᴘʀᴏʜɪʙɪᴅᴏ ʜᴀʙʟᴀʀ ᴀʟ ᴘʀɪᴠᴀᴅᴏ ᴅᴇʟ 💋Namecita 💞🏴‍☠️ ᴘᴏʀ ʟᴏ ᴄᴜᴀʟ sᴇʀᴀs ʙʟᴏϙᴜᴇᴀᴅᴏ.\n\n𝑺𝒊 𝒒𝒖𝒊𝒆𝒓𝒆𝒔 𝒃𝒐𝒕, 𝒊𝒏𝒕𝒆𝒓𝒄𝒂𝒎𝒃𝒊𝒂𝒓 𝒏𝒖𝒎𝒆𝒓𝒐 𝒙 𝒃𝒐𝒕 𝒑𝒆𝒓𝒔𝒐𝒏𝒂𝒍𝒊𝒛𝒂𝒅𝒐±𝑪𝒖𝒆𝒏𝒕𝒂 𝒅𝒆 𝒔𝒕𝒓𝒆𝒂𝒎𝒊𝒏𝒈:\n\n𝐇𝐚𝐛𝐥𝐚𝐫 𝐚: Wa.me/56986469687`, false, {mentions: [m.sender]});
+    await m.reply(`*[❗] ʜᴏʟᴀ @${m.sender.split`@`[0]}, єѕтα ρяσнιвι∂σ нαвℓαя αℓ ρяινα∂σ ∂є 💋Ɲ4MЄƇƖƬ4_ƁƇ 💞 🚫 ᴘᴏʀ ʟᴏ ᴄᴜᴀʟ sᴇʀᴀs ʙʟᴏϙᴜᴇᴀᴅᴏ.\n\nѕι qυιєяєѕ вσт ραяα тυѕ gяυρσѕ σ вσт ρєяzσηαℓιzα∂σѕ:\n\n𝐇𝐚𝐛𝐥𝐚𝐫 𝐚: Wa.me/56936399966`, false, {mentions: [m.sender]});
     await this.updateBlockStatus(m.chat, 'block');
   }
   return !1;

2532c887eeb89ab53d61a94874ebe42f0a9681ec
n4mecit4-CB <151418600+n4mecit4-CB@users.noreply.github.com>
2023-11-19 02:54:17 -0300

    𝘯4𝘮𝘦𝘤𝘪𝘵4-𝘣𝘤

--- index.js
+++ index.js
@@ -13,11 +13,11 @@ const {name, author} = require(join(__dirname, './package.json'));
 const {say} = cfonts;
 const rl = createInterface(process.stdin, process.stdout);
 
-say('C4rLa\nWhatsApp Bot MD', {
+say('N4MECIT4\nWhatsApp Bot MD', {
   font: 'chrome',
   align: 'center',
   gradient: ['red', 'magenta']});
-say(`Bot creado por @n4mecit4off`, {
+say(`Bot creado por @n4mecit4_bc`, {
   font: 'console',
   align: 'center',
   gradient: ['red', 'magenta']});

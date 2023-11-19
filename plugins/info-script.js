ee2bdf050381f44a00a260210deb41924263698e
n4mecit4-CB <151418600+n4mecit4-CB@users.noreply.github.com>
2023-11-19 05:49:58 -0300

    Ɲ4MЄƇƖƬ4_ƁƇ

--- plugins/info-script.js
+++ plugins/info-script.js
@@ -3,12 +3,12 @@ import fetch from 'node-fetch';
 const handler = async (m, { conn, args }) => {
    const res = await fetch('https://api.github.com/repos/BrunoSobrino/TheMystic-Bot-MD');
    const json = await res.json();
-   let txt = `           *乂 𝕵𝖔𝖘𝖘 𝕭𝖔𝖙 🏴‍☠️ 乂*\n\n`;
-      txt += `◦  *Nombre:* ${json?.name || '𝕵𝖔𝖘𝖘 𝕭𝖔𝖙 🏴‍☠️'}\n`;
+   let txt = `           *乂 Ɲ4MЄƇƖƬ4_ƁƇ 乂*\n\n`;
+      txt += `◦  *Nombre:* ${json?.name || 'Ɲ4MЄƇƖƬ4_ƁƇ'}\n`;
       txt += `◦  *Visitantes:* ${json?.watchers_count || '-'}\n`;
       txt += `◦  *Tamaño:* ${(json?.size / 1024).toFixed(2) || '-'} MB\n`;
       txt += `◦  *Actualización:* ${moment(json?.updated_at).format('DD/MM/YY - HH:mm:ss') || '-'}\n`;
-      txt += `◦  *Url:* ${json?.html_url || 'https://www.instagram.com/ancelzzz/'}\n\n`;
+      txt += `◦  *Url:* ${json?.html_url || 'https://www.instagram.com/n4mecit4_bc/'}\n\n`;
       txt += `	 ${json?.forks_count || '-'} Forks · ${json?.stargazers_count || '-'} Stars · ${json?.open_issues_count || '-'} Issues`;
    await conn.sendMessage(m.chat, {text: txt.trim(), mentions: [...txt.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net'), contextInfo: {forwardingScore: 9999999, isForwarded: true, mentionedJid: [...txt.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net'), "externalAdReply": {"showAdAttribution": true, "containsAutoReply": true, "renderLargerThumbnail": true, "title": global.titulowm2, "containsAutoReply": true, "mediaType": 1, "thumbnail": imagen6, "mediaUrl": `https://www.atom.bio/theshadowbrokers-team`, "sourceUrl": `https://www.atom.bio/theshadowbrokers-team`}}}, {quoted: m});
 };

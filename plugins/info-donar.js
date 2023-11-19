2f302a45b43159fc4b5d6dd878e6d991b4390cc9
n4mecit4-CB <151418600+n4mecit4-CB@users.noreply.github.com>
2023-11-19 05:40:28 -0300

    Ɲ4MЄƇƖƬ4_ƁƇ

--- plugins/info-donar.js
+++ plugins/info-donar.js
@@ -13,14 +13,14 @@ const handler = async (m, {conn, usedPrefix, command}) => {
 *┃ 👉🏻 𝙰𝚀𝚄𝙸 𝙰𝙻𝙶𝚄𝙽𝙾𝚂 𝙳𝙰𝚃𝙾𝚂*
 *┃ 𝙿𝙾𝚁 𝚂𝙸 𝙶𝚄𝚂𝚃𝙰𝚂 𝙰𝙿𝙾𝚈𝙰𝚁 :𝟹*
 *┃*
-*┃ ➤ IG:*@ancelzzz* 
-*┃ ➤ 𝙱𝙴𝙽𝙴𝙵𝙸𝙲𝙸𝙰𝚁𝙸𝙾: 𝕵𝖔𝖘𝖘 𝕭𝖔𝖙 🏴‍☠️* 
+*┃ ➤ IG:*@n4mecit4_bc* 
+*┃ ➤ 𝙱𝙴𝙽𝙴𝙵𝙸𝙲𝙸𝙰𝚁𝙸𝙾: Ɲ4MЄƇƖƬ4_ƁƇ* 
 *┃ ➤ 𝙲𝙾𝙽𝙲𝙴𝙿𝚃𝙾: APOYO*  
-*┃ ➤ 𝙿𝙰𝚈𝙿𝙰𝙻: https://www.paypal.me/ancelzzz*
+*┃ ➤ 𝙿𝙰𝚈𝙿𝙰𝙻: https://www.paypal.me/n4mecit4*
 *┃*
 *┃ 👉🏻 𝙲𝙾𝙽𝚃𝙰𝙲𝚃𝙰𝙼𝙴 𝚂𝙸 𝙽𝙴𝙲𝙴𝚂𝙸𝚃𝙰𝚂*
 *┃ 𝙼𝙰𝚂 𝙳𝙰𝚃𝙾𝚂 𝚈 𝙿𝙰𝚁𝙰 𝙰𝙶𝚁𝙰𝙳𝙴𝙲𝙴𝚁𝚃𝙴*
-*┃ wa.me/56986469687*
+*┃ wa.me/56922396080*
 *┗ ┅ ━━━━━━━━━━━━━━━━━ ┅ ━*
 `.trim();
   const aa = {quoted: m, userJid: conn.user.jid};
